bf5022aeb96e4dc016cf940657617b5ac9bce73d
Matija8 <matijanme@gmail.com>
2020-02-07 21:56:38 +0100

    Ommited manual video stitching

--- dl_choose_type.py
+++ dl_choose_type.py
@@ -6,66 +6,35 @@ import subprocess
 
 
 def main():
-    n_of_arguments = len(sys.argv)
-    if n_of_arguments != 2:
-        exit(f"Must have 1 argument (URL)! {n_of_arguments-1} given.")
+    if len(sys.argv) < 2:
+        exit(f"Must have at least 1 argument (URL)!")
 
-    # ampersand problemi! Izbegevaj zbog shell-a
-    URL = sys.argv[1]
-    amp_pos = URL.find('&')
-    if amp_pos > 0:
-        #URL = URL[0:amp_pos]
-        exit("URL can't have ampersands! (Shell issues)")
-    cmd = f"youtube-dl -F {URL}"
-    run_cmd(cmd, "Formats available:\n")
+    # Avoid ampersands (shell issues), or put URL-s in quotes.
+    URLs = sys.argv[1:]
 
+    for URL in URLs:
 
-    video_format = input("\nEnter video format: ")
-    audio_format = input("\nEnter audio format: ")
+        # Get formats:
+        print(f"Parsing URL: {URL}")
+        print("Formats available:\n")
+        command = f"youtube-dl -F {URL}".split()
+        subprocess.run(command)
 
-    cmd = f"youtube-dl -f {video_format} {URL} -o %(title)s.%(ext)s"
-    run_cmd(cmd,"\nDownloading video...")
+        video_format = input("\nEnter video format: ")
+        audio_format = input("\nEnter audio format: ")
 
-    cmd = f"youtube-dl -f {video_format} --get-filename -o %(title)s.%(ext)s {URL}"
-    out = subprocess.check_output(cmd.split())
-    video = out.decode("utf-8").strip()
-
-    cmd = f"youtube-dl -f {audio_format} {URL} -o %(title)s.%(ext)s"
-    run_cmd(cmd, "\nDownloading audio...")
-
-    cmd = f"youtube-dl -f {audio_format} --get-filename -o %(title)s.%(ext)s {URL}"
-    out = subprocess.check_output(cmd.split())
-    audio = out.decode("utf-8").strip()
-
-    #STITCH
-
-
-    cmd = f"ffmpeg -i -i -c:v copy -c:a aac -strict experimental"
-    cmd = cmd + " -loglevel panic"
-    cmd = cmd + " -hide_banner"
-    cmd = cmd.split()
-    cmd.insert(2, video)
-    cmd.insert(4, audio)
-    output_name = "DL - " + video
-    cmd.append(output_name)
-
-    run_cmd(cmd,"\nStitching video & audio...")
-
-    run_cmd(['rm', video, audio], "\nRemoving leftover files...")
-
+        command = f"youtube-dl -f {video_format}+{audio_format} {URL} -o %(title)s.%(ext)s".split()
+        start = time.time()
+        print(f"Getting video from url: {URL}")
+        try:
+            subprocess.run(command)
+        except subprocess.CalledProcessError:
+            print("No given format.")
+        end = time.time()
+        print(f"Time elapsed: {end - start:.2f} seconds")
+    print("All done!")
     return
-
-
-def run_cmd(cmd = "echo 'No command!'", msg = ""):
-    start = time.time()
-    print(msg)
-    if isinstance(cmd, str):
-        cmd = cmd.split()
-    #print(cmd)
-    subprocess.run(cmd)
-    end = time.time()
-    print(f"Time elapsed: {end - start:.2f} seconds")
-    return
+    
 
 if __name__ == "__main__":
     main()
